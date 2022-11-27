dcf708616cee16bcefcae2430c5bcd6d8f7c84e6
EduRamos95 <eduramos08.30@gmail.com>
2022-11-27 01:21:42 -0500

    feat: update state.py for SQLAlchemy

--- models/state.py
+++ models/state.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from models import storage
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+import os
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    if os.getenv("HBNB_TYPE_STORAGE") == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascade="all, delete, delete-orphan")
+    else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    @property
+    def cities(self):
+        """return a list of city instances with state_id = current"""
+        all_instances = storage.all(City)
+        query = []
+        for key, value in all_instances.items():
+            if getattr(value, 'state_id') == self.id:
+                query.append(value)
+        return query
