df7786c764944bcb7f02b050d900941fd3e1f011
gustavofranco26 <gustavo260123@gmail.com>
2022-12-02 08:10:45 -0500

    Adding the pep8 valid to file.

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,10 +9,12 @@ def hello_hbnb():
     """Module that display"""
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Module that display"""
     return 'HBNB'
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000, debug=True)
